3670400e89a567d1d380fd7473eb468f7335f107
Erick J. Viera <eric.viera@acklenavenue.com>
2022-09-02 17:36:13 -0600

    pulled default-sg's rules into sg-rule resources

--- modules/vpc/vpc-resources.tf
+++ modules/vpc/vpc-resources.tf
@@ -75,20 +75,24 @@ resource "aws_route_table_association" "private" {
 
 resource "aws_security_group" "default" {
   name        = "${var.environment}-default-sg"
-  description = "Default SG to alllow traffic only from inside itself"
+  description = "Default SG to allow traffic only from inside itself"
   vpc_id      = aws_vpc.main.id
+}
 
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
+resource "aws_security_group_rule" "self-ingress" {
+  security_group_id = aws_security_group.default.id
+  type = "ingress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+  self      = true
+}
 
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
+resource "aws_security_group_rule" "self-egress" {
+  security_group_id = aws_security_group.default.id
+  type = "egress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+  self      = true
 }
