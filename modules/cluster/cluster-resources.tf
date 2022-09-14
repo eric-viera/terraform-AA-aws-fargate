ca8650b48a2c343ebd12493d1179c465ad1873b9
Erick J. Viera <eric.viera@acklenavenue.com>
2022-09-14 12:13:39 -0600

    fixed broken cluster naming

--- modules/cluster/cluster-resources.tf
+++ modules/cluster/cluster-resources.tf
@@ -60,7 +60,7 @@ resource "aws_iam_instance_profile" "ecs_agent" {
 }
 
 resource "aws_ecs_cluster" "main" {
-  name = "${var.project_name}-cluster"
+  name = "${var.project_name}-${var.environment}-cluster"
 
   setting {
     name  = "containerInsights"
