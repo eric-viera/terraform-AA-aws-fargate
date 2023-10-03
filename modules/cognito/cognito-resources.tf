d3a5ade9d7a6196401a71120c19487ee0a1d64d0
Eric Viera <eric.viera@acklenavenue.com>
2023-10-03 09:52:09 -0600

    add boilerplate verification messaging

--- modules/cognito/cognito-resources.tf
+++ modules/cognito/cognito-resources.tf
@@ -1,24 +1,42 @@
 resource "aws_cognito_user_pool" "users-pool" {
-  name = "${var.name}-user-pool"
+  name                     = "${var.name}-user-pool"
+  auto_verified_attributes = [ "email" ]
+  mfa_configuration        = "OPTIONAL"
+  username_attributes = [ "email" ]
   account_recovery_setting {
     recovery_mechanism {
-      name = "verified_email"
+      name     = "verified_email"
       priority = 1
     }
   }
-  mfa_configuration = "OPTIONAL"
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+    invite_message_template {
+      email_message = "Welcome to the webapp, {username}, your temporay password is: {####}."
+      email_subject = "Webapp invitation"
+      sms_message   = "Welcome to the webapp, {username}, your temporay password is: {####}."
+    }
+  }
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
+  }
   password_policy {
-    minimum_length = 8
+    minimum_length    = 8
     require_lowercase = true
-    require_numbers = true
-    require_symbols = true
+    require_numbers   = true
+    require_symbols   = true
     require_uppercase = true
   }
   software_token_mfa_configuration {
     enabled = true
   }
-  email_configuration {
-    email_sending_account = "COGNITO_DEFAULT"
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_LINK"
+    email_message = "This is your email address confirmation code: {####}"
+    email_message_by_link = "{##Click Here##} to confirm your email address"
+    email_subject = "confirmation code"
+    email_subject_by_link = "Confirmation link"
+    sms_message = "Your verification code is {####}"
   }
 }
 
